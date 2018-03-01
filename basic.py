36cc9a9e483708c4dbf4ae181517af809cc26074
Kristoffer Hilmersson <kristoffer.hilmersson@volvocars.com>
2018-03-01 19:15:03 +0100

    a parsing start

--- basic.py
+++ basic.py
@@ -1,2 +1,23 @@
+
+def parse_dataset(data):
+
+	rows, cols, cars, rides, bonus, steps = map(int, data.readline().strip().split())
+
+	rides = []
+
+	for ride in range(rows):
+		row_start, col_start, row_fin, col_fin, start, finish = map(int, data.readline().strip().split())
+		ride = {'row_start': row_start, 'col_start': col_start, 'row_fin': row_fin, 'col_fin': col_fin, 'start': start, 'finish': finish}
+		rides.append(ride)
+
+	return rides
+
 def main():
-	#start parsing
+	file = 'a_example.in'
+	with open(file) as f:
+		rides = parse_dataset(f)
+	
+
+
+if __name__ == "__main__":
+    main()
